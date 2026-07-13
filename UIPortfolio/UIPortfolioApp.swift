f131fea1b7310dcec88e677ebc70e81ae8c620b0
asaelrddz <asaelrddz00@gmail.com>
2026-07-12 21:54:49 -0600

    Initial Commit

---
+++ UIPortfolio/UIPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIPortfolioApp.swift
+//  UIPortfolio
+//
+//  Created by Asael Rodriguez on 12/07/26.
+//
+
+import SwiftUI
+
+@main
+struct UIPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
